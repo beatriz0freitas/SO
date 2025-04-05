befefa226522cd94c37b38a86a7e09e5f8e10bda
Alice-Soares <alice.faria.soares@gmail.com>
2025-04-05 10:09:03 +0100

    correçao

--- include/MetaInformationDataset.h
+++ include/MetaInformationDataset.h
@@ -1,6 +1,9 @@
 #ifndef METAINFORMATIONDATASET_C
 #define METAINFORMATIONDATASET_C
 
+#include "MetaInformation.h"
+#include <glib.h>
+
 typedef struct MetaInformationDataset MetaInformationDataset;
 MetaInformationDataset *MetaInformationDataset_new();
 
