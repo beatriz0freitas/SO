9e1f84ad4b8c156045ce3295850ee6b91cb635b6
Alice-Soares <alice.faria.soares@gmail.com>
2025-04-05 10:10:08 +0100

    passei struct para .h

--- include/metaInformation.h
+++ include/metaInformation.h
@@ -1,13 +1,7 @@
 #ifndef META_INFORMATION_H
 #define META_INFORMATION_H
 
-typedef struct information{
-    int idDocument;         //TODO: temos de definir como vamos catalogar
-    char documentTitle[200];
-    char author[200];         //separados por ;
-    int year;
-    char path[64];
-    char keywords[64];
-} Information;
+typedef struct Information Information;
+
 
 #endif
