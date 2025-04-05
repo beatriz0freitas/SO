e26e3562950435dd7da86799072f863851570f95
beatriz0freitas <a106853@alunos.uminho.pt>
2025-04-05 09:32:32 +0100

    modulos para guardar meta informacao

---
+++ include/metaInformation.h
@@ -0,0 +1,14 @@
+#ifndef META_INFORMATION_H
+#define META_INFORMATION_H
+
+typedef struct information{
+    //char fifo_client    -  nome do FIFO de resposa do cliente
+    int idDocument;         //TODO: temos de definir como vamos catalogar
+    char documentTitle[200];
+    char author[200];         //separados por ;
+    int year;
+    char path[64];
+    char keywords[64];
+} Information;
+
+#endif
--- include/utils.h
+++ include/utils.h
@@ -1,18 +1,5 @@
 #ifndef UTILS
 #define UTILS
 
-#define SERVER "fifo_server"
-#define CLIENT "fifo_client"
-
-typedef struct information{
-    //char fifo_client    -  nome do FIFO de resposa do cliente
-    int idDocument;         //TODO: temos de definir como vamos catalogar
-    char documentTitle[200];
-    char author[200];         //separados por ;
-    int year;
-    char path[64];
-    char keywords[64];
-} Information;
-
 
 #endif 
