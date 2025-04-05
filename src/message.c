571205cb2b036e5c05a8ab4effda3f97044e275c
beatriz0freitas <a106853@alunos.uminho.pt>
2025-04-05 13:56:45 +0100

    implementacao modulo messages

---
+++ src/message.c
@@ -0,0 +1,48 @@
+#include "message.h"
+
+struct Message{
+    Command *cmd;
+    MetaInformation *info;
+};
+
+Message *message_new(Command *cmd, MetaInformation *info) {
+    Message *msg = (Message *)malloc(sizeof(Message));
+    if (!msg) {
+        perror("Erro ao alocar memória para a mensagem");
+        exit(1);
+    }
+    msg->cmd = cmd;
+    msg->info = info;
+    return msg;
+}
+
+void message_free(Message *msg) {
+    if (msg) {
+        command_free(msg->cmd);
+        metaInformation_free(msg->info);
+        free(msg);
+    }
+}
+
+void message_set_command(Message *msg, Command *cmd) {
+    if (msg) {
+        msg->cmd = cmd;
+    }
+}
+void message_set_metaInformation(Message *msg, MetaInformation *info) {
+    if (msg) {
+        msg->info = info;
+    }
+}
+Command *message_get_command(Message *msg) {
+    if (msg) {
+        return msg->cmd;
+    }
+    return NULL;
+}
+MetaInformation *message_get_metaInformation(Message *msg) {
+    if (msg) {
+        return msg->info;
+    }
+    return NULL;
+}
