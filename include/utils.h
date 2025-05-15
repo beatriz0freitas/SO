17980107ab6e94ac07e70936adc5a1f63665b126
Alice-Soares <alice.faria.soares@gmail.com>
2025-05-15 16:21:23 +0000

    servidor em paralelo

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,9 @@
 #include <string.h>
 #include <sys/types.h>
 
+#define FIFO_CLIENT_TO_SERVER "fifos/clientToServer"
+#define FIFO_TEMPLATE_SERVER_TO_CLIENT "fifos/serverToClient_%d"
+
 ssize_t bufferedRead(int fd, void *buf, size_t count);
 ssize_t bufferedWrite (int fd, const void *buf, size_t count);
 int mysystem(const char *command_line);
